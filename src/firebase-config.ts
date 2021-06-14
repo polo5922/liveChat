d6ebc4144ec6bb717c852ada3915c2b7a1cf5f3a
polo5922 <paul.barbaux@gmail.com>
2021-06-14 16:52:09 +0200

    first commit

---
+++ src/firebase-config.ts
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyCcDK1TK7nqsKywoqqIpFmnYB_jx0M_QP8",
+    authDomain: "chat-e4dfe.firebaseapp.com",
+    projectId: "chat-e4dfe",
+    storageBucket: "chat-e4dfe.appspot.com",
+    messagingSenderId: "498302546440",
+    appId: "1:498302546440:web:62c9c55c97ef8584d997bd",
+    measurementId: "G-TRTSJP0Z23"
+}
